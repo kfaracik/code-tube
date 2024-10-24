0972ddf85a4fdae96723eea684bbd4ceea657a65
Krzysztof <larkfreeme70.55@gmail.com>
2024-10-24 19:22:23 +0200

    refactor(translation): use translation

--- src/navigation/navigators/home/bottom-tab-navigator/BottomTabNavigator.tsx
+++ src/navigation/navigators/home/bottom-tab-navigator/BottomTabNavigator.tsx
@@ -4,11 +4,14 @@ import { HomeNavigator } from "../home";
 import { SearchNavigator } from "../search";
 import type { BottomTabNavigatorParams } from "@navigation/types";
 import { BottomTabs } from "@navigation/constants";
+import { useTranslation } from "react-i18next";
 
 const { Navigator, Screen } =
   createBottomTabNavigator<BottomTabNavigatorParams>();
 
 export const BottomTabNavigator = () => {
+  const { t } = useTranslation();
+
   return (
     <Navigator
       screenOptions={{
@@ -19,14 +22,14 @@ export const BottomTabNavigator = () => {
         name={BottomTabs.Home}
         component={HomeNavigator}
         options={{
-          title: "Home",
+          title: t("bottomBar.home"),
         }}
       />
       <Screen
         name={BottomTabs.Search}
         component={SearchNavigator}
         options={{
-          title: "Search",
+          title: t("bottomBar.search"),
         }}
       />
     </Navigator>
