6ccf27920da23f5e8e6a00c4e874e316c10495f1
Krzysztof <larkfreeme70.55@gmail.com>
2024-10-29 14:48:37 +0100

    refactor: refactor code

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import { AppContent } from "@navigation/app-content";
 import { I18nextProvider } from "react-i18next";
-import i18n from '@lib/i18n';
+import i18n from "@lib/i18n";
 
 const App = () => {
   return (
